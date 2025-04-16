5f19131116766c5858b655a998311960e0d7c7ad
Guilherme Rosa <guilherme_rozza@hotmail.com>
2025-04-16 05:18:55 -0300

    FEAT - Adicionado o status do atestado medico
    Foi adicionado o status do atestado medico ao gerenciamento de atestados do aluno e dos docente. Diz se o atestado foi aprovado, recusado ou se está pendente.

--- src/lista_atestados.py
+++ src/lista_atestados.py
@@ -2,7 +2,7 @@ atestados = [
     {"Nome": "João", "Turma": '2°', "RA": 100001, "Tipo": 'Maternidade', "Inicio": 'xx.xx.xxxx', "Fim": 'xx.xx.xxxx', 
      "Horas": 'xx:xx - xx:xx', "Status": 'Pendente', "Motivo": 'Recusado por erro de dados', "Comprovante": "ID do comprovante", "ID": 1},
     {"Nome": "Yuri", "Turma": '3°', "RA": 100002, "Tipo": 'Maternidade', "Inicio": 'xx.xx.xxxx', "Fim": 'xx.xx.xxxx', 
-     "Horas": 'xx:xx - xx:xx', "Status": 'Pendente', "Motivo": '-', "Comprovante": "ID do comprovante", "ID": 2},
+     "Horas": 'xx:xx - xx:xx', "Status": True, "Motivo": '-', "Comprovante": "ID do comprovante", "ID": 2},
     {"Nome": "Leonardo", "Turma": '1°', "RA": 100002, "Tipo": 'Consulta', "Inicio": 'xx.xx.xxxx', "Fim": 'xx.xx.xxxx', 
-     "Horas": 'xx:xx - xx:xx', "Status": 'Pendente', "Motivo": '-', "Comprovante": "ID do comprovante", "ID": 2}
+     "Horas": 'xx:xx - xx:xx', "Status": False, "Motivo": '-', "Comprovante": "ID do comprovante", "ID": 2}
 ]
