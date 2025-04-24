ec17d8e50718c38054b75d69534d04fb89ea4652
gustav0-gg <gugugarciaaa+github@gmail.com>
2025-04-23 23:39:11 -0300

    FEAT - Adicionando a criação automática do arquivo usuarios.json
    Código para a criação do arquivo de usuários para o funcionamento do login/cadastro

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,url_for
 import os
 import time
+import json
 
 app = Flask(__name__)
 app.secret_key = '4657'
@@ -10,6 +11,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 from views import *
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+dir_base = os.path.dirname(os.path.abspath(__file__))
+caminho_usuarios = os.path.join(dir_base, 'usuarios.json')
+
+if not os.path.exists(caminho_usuarios):
+    with open(caminho_usuarios, 'w') as u:
+        json.dump({}, u)
 
 if __name__ == '__main__' :
     app.run(debug=True)
