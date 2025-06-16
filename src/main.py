2364e254b0433950d890dd8a5e91c36c7eb99b03
Gabrielle Novaes <gabrielle.goncalves4@fatec.sp.gov.br>
2025-06-16 20:01:14 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -36,5 +36,7 @@ if not os.path.exists(caminho_equipes):
     with open(caminho_equipes, 'w') as u:
         json.dump({}, u)
 
-if __name__ == '__main__' :
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
