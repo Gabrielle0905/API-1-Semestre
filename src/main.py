78568c44cfc342f98086b116317807f4fb534c39
Gabrielle Novaes <gabrielle.goncalves4@fatec.sp.gov.br>
2025-06-16 20:43:37 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -38,5 +38,6 @@ if not os.path.exists(caminho_equipes):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    print("Porta que o app vai usar:", port)
     app.run(host='0.0.0.0', port=port, debug=True)
 
